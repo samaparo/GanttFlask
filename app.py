e5048135190001702d9c15876d773f28c6a9d5a0
Sam <mirthquakes@gmail.com>
2013-11-22 20:03:23 -0600

    importing sqlalchemy and built basic task class

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 from flask import Flask, jsonify, request
-app = Flask(__name__)
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	name = db.Column(db.String(256))
+	start = db.Column(db.Date)
+	end = db.Column(db.Date)
+	
+	def __init__(self, name, start, end):
+		self.name = name
+		self.start = start
+		self.end = end
+	
 @app.route('/api/tasks/', methods=['GET'])
 def getAllTasks():
 	return jsonify({'tasks':['A', 'B']})
